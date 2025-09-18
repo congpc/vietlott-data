6cb30d67953d22d68ff098fcf213a2f61d952efc
Pham Chi Cong <congpc@trans-tech.vn>
2025-09-18 14:21:46 +0700

    Update data

--- bin/github_data.sh
+++ bin/github_data.sh
@@ -8,6 +8,9 @@ USER="Cong Pham"
 EMAIl="chicong7891@gmail.com"
 VENV=".venv"
 
+pwd
+ls -a
+
 # Activate virtual environment if it exists
 if [ -n "$VENV" ]; then
   source "$VENV/bin/activate"
@@ -19,20 +22,20 @@ echo "pwd $(pwd)"
 export PYTHONPATH="src"
 export LOGURU_LEVEL="INFO"
 
-python src/vietlott/cli/crawl.py keno
-python src/vietlott/cli/missing.py keno
-python src/vietlott/cli/crawl.py power_655
-python src/vietlott/cli/missing.py power_655
-python src/vietlott/cli/crawl.py power_645
-python src/vietlott/cli/missing.py power_645
+#python src/vietlott/cli/crawl.py keno
+#python src/vietlott/cli/missing.py keno
+#python src/vietlott/cli/crawl.py power_655
+#python src/vietlott/cli/missing.py power_655
+#python src/vietlott/cli/crawl.py power_645
+#python src/vietlott/cli/missing.py power_645
 python src/vietlott/cli/crawl.py power_535
 python src/vietlott/cli/missing.py power_535
-python src/vietlott/cli/crawl.py 3d
-python src/vietlott/cli/missing.py 3d
-python src/vietlott/cli/crawl.py 3d_pro
-python src/vietlott/cli/missing.py 3d_pro
-python src/vietlott/cli/crawl.py bingo18
-python src/vietlott/cli/missing.py bingo18
+#python src/vietlott/cli/crawl.py 3d
+#python src/vietlott/cli/missing.py 3d
+#python src/vietlott/cli/crawl.py 3d_pro
+#python src/vietlott/cli/missing.py 3d_pro
+#python src/vietlott/cli/crawl.py bingo18
+#python src/vietlott/cli/missing.py bingo18
 
 python src/render_readme.py
 
